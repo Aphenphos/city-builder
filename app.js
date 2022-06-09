a08b1234d16d9bcd74a7acf88d0895d583412aac
Aphenphos <MaxwillWinters@gmail.com>
2022-06-09 18:50:19 -0400

    added js for city elements

--- app.js
+++ app.js
@@ -1,15 +1,78 @@
-const cities = [];
-const slogans = [];
-const submitNewCity = document.getElementById('submit-new-city');
+function defaultCity() {
+    const defaultCity = {
+        name: 'Sahara',
+        slogan: 'This place is dope.',
+        environment: 'Tropical',
+        architecture: 'Romanesque'
+    };
+    return defaultCity;
+}
 
-var userCityName = document.querySelector('user-city-name');
-var userCitySlogan = document.querySelector('user-city-slogan');
+let city = defaultCity();
+const citiesAndSlogans = [];
 
 
-submitNewCity.addEventListener('click', () => {
-    cities.push(userCityName);
-    slogans.push(userCitySlogan);
-    console.log(cities);
-    console.log(slogans);
+
+const inputs = document.getElementById('inputs');
+const display = document.getElementById('display');
+
+
+
+var cityName = inputs.querySelector('user-city-name');
+var citySlogan = inputs.querySelector('user-city-slogan');
+var cityEnvironmentSelector = inputs.querySelector('city-environment');
+var cityArchitectureSelector = inputs.querySelector('city-architecture');
+
+function displayInputs() {
+    cityName.value = city.name;
+    citySlogan.value = city.slogan;
+    cityEnvironmentSelector.value = city.environment;
+    cityArchitectureSelector.value = city.architecture;
+}
+
+cityName.addEventListener('input', () => {
+    city.name = cityName.value;
+    displayCitiesAndSlogans();
 });
 
+citySlogan.addEventListener('input', () => {
+    city.name = citySlogan.value;
+    displayCitiesAndSlogans();
+});
+
+cityEnvironmentSelector.addEventListener('change', () => {
+    city.environment = cityEnvironmentSelector.value;
+    displayCitiesAndSlogans();
+});
+
+cityArchitectureSelector.addEventListener('change', () => {
+    city.architecture = cityArchitectureSelector.value;
+    displayCitiesAndSlogans();
+});
+
+
+
+const list = display.querySelector('ul');
+
+function displayCitiesAndSlogans() {
+    list.innerHTML = '';
+    for (const cityAndSlogan of citiesAndSlogans) {
+
+        const li = document.createElement('li');
+        const cityName = document.createElement('h1');
+        cityName.textContent = cityName;
+        const citySlogan = document.createElement('h2');
+        citySlogan.textContent = citySlogan;
+        const cityEnvironment = document.createElement('h3');
+        cityEnvironment.textContent = cityEnvironmentSelector;
+        const cityArchitecture = document.createElement('h3');
+        cityArchitecture.textContent = cityArchitectureSelector;
+
+        li.append(cityName, citySlogan, cityEnvironment, cityArchitecture);
+        list.push(li);
+        displayCitiesAndSlogans();
+    }
+
+}
+
+displayCitiesAndSlogans();
