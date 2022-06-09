089a28835fd57ff861d20af432f6fc7657d80c73
Aphenphos <MaxwillWinters@gmail.com>
2022-06-08 20:41:17 -0400

    index setup and arrays made

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-// import needed modules
+const cities = [];
+const slogans = [];
+const submitNewCity = document.getElementById('submit-new-city');
 
-// state
+var userCityName = document.querySelector('user-city-name');
+var userCitySlogan = document.querySelector('user-city-slogan');
 
-// components
-    // component
-    // define and grab DOM elements
-    // display functions
-    // optional: subscribe to events
-        // event handlers - what needs to happen?
-        // logic and calculations
-        // state update
-        // re-display components (which ones?)
-    // optional: handle functions for shared event handler logic
 
-// page load actions
-
+submitNewCity.addEventListener('click', () => {
+    cities.push(userCityName);
+    slogans.push(userCitySlogan);
+    console.log(cities);
+    console.log(slogans);
+});
 
